89ea8dc9278e5565b65dbb6d36ec5ac1e1f49fd8
anton_remezovskyi2508 <antonremezovskyi@gmail.com>
2024-10-06 09:30:37 +0300

    change formik on default input

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,22 +1,19 @@
 import css from './SearchBox.module.css';
 
 import { useId } from 'react';
-import { Formik, Field } from 'formik';
 
 export default function SearchBox({ searchValue, setSearchValue }) {
   const searchInputId = useId();
   return (
-    <Formik initialValues={searchValue} onSubmit={() => {}}>
-      <div className={css.wrapper}>
-        <label htmlFor={searchInputId}>Find contacts by name</label>
-        <Field
-          type="text"
-          name="searchName"
-          value={searchValue}
-          id={searchInputId}
-          onChange={event => setSearchValue(event.target.value)}
-        />
-      </div>
-    </Formik>
+    <div className={css.wrapper}>
+      <label htmlFor={searchInputId}>Find contacts by name</label>
+      <input
+        type="text"
+        name="searchName"
+        value={searchValue}
+        id={searchInputId}
+        onChange={event => setSearchValue(event.target.value)}
+      />
+    </div>
   );
 }
